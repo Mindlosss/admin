b130dde5a4d68219aaa6e57b143b63c447b4f711
Mindlosss <projectal4n@gmail.com>
2025-12-15 15:57:27 -0600

    Minor changes

--- admin/views/layouts/footer.php
+++ admin/views/layouts/footer.php
@@ -2,15 +2,15 @@
     <div class="page-container">
         <div class="row">
             <div class="col-md-6 text-center text-md-start">
-                <script>document.write(new Date().getFullYear())</script> © Abstack - By <span class="fw-bold text-decoration-underline text-uppercase text-reset fs-12">Coderthemes</span>
+                <script>document.write(new Date().getFullYear())</script> © Código y chips
             </div>
-            <div class="col-md-6">
+            <!-- <div class="col-md-6">
                 <div class="text-md-end footer-links d-none d-md-block">
                     <a href="javascript: void(0);">About</a>
                     <a href="javascript: void(0);">Support</a>
                     <a href="javascript: void(0);">Contact Us</a>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </footer>
